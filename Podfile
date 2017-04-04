1deb24e0ebb27e5ff42af56c3c2133f4adce225a
Luis Fernando Bustos Ramírez <lufe.bura@gmail.com>
2017-04-03 22:44:47 -0500

    Has been added the Xcode project

---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target ‘squirrelTrip’ do
+	pod 'Cosmos', '~> 7.0'
+	pod 'Firebase/Core'
+end
---
+++ SquirrelTrip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SquirrelTrip
+//
+//  Created by Luis Fernando Bustos Ramírez on 3/21/17.
+//  Copyright © 2017 Luis Fernando Bustos Ramírez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
